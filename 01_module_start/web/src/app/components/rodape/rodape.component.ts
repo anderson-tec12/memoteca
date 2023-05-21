f95e52b3bb58016770149a291e20270763fe071e
Anderson Barros Silva <anderson.tec12@gmail.com>
2023-05-20 21:50:24 -0300

    Rodape finalizado

--- 01_module_start/web/src/app/components/rodape/rodape.component.ts
+++ 01_module_start/web/src/app/components/rodape/rodape.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rodape.component.css']
 })
 export class RodapeComponent implements OnInit {
-
+  year = new Date().getFullYear()
   constructor() { }
 
   ngOnInit(): void {
